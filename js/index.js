fe92bb100c1d3d8a7526f6b3df797dcd0ed257f9
junnatc <93339775+junnatc@users.noreply.github.com>
2024-07-24 09:35:29 -0400

    Delete js directory

--- js/index.js
+++
@@ -1,5 +0,0 @@
-// Naviagtes to Game Page
-function navigateToDestination() {
-    window.location.href = './pages/gamePage.html';
-// Game Page Code
-let correctChoices = document.querySelector("choices")
