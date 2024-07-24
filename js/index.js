8d91413f35eec4c67abe6ad20f054ffe75423636
junnatc <93339775+junnatc@users.noreply.github.com>
2024-07-24 09:36:25 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,7 @@
+// Naviagtes to Game Page
+function navigateToDestination() {
+    window.location.href = './pages/gamePage.html';
+}
+function navigateToGamePage() {
+    window.location.href = '../index.html';
+}
